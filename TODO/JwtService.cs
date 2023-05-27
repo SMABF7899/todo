25bef9db1e356a9b2dfc69807cd2e5964ad458f9
Seyed Mohammad Ali Bani Fatemi <smabanifatemi@gmail.com>
2023-05-27 13:51:20 +0430

    Add JwtService class

---
+++ TODO/JwtService.cs
@@ -0,0 +1,40 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TODO;
+
+public class JwtService
+{
+    private readonly string _secret;
+    private readonly string _issuer;
+    private readonly string _audience;
+
+    public JwtService(string secret, string issuer, string audience)
+    {
+        _secret = secret;
+        _issuer = issuer;
+        _audience = audience;
+    }
+
+    public string GenerateSecurityToken(string username)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var symmetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secret));
+        var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha512Signature);
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, username)
+            }),
+            Expires = DateTime.UtcNow.AddDays(1),
+            Issuer = _issuer,
+            Audience = _audience,
+            SigningCredentials = signingCredentials
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+    }
+}
