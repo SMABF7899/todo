8d9f8976b0a3757b54644b4182858d92978babae
Seyed Mohammad Ali Bani Fatemi <smabanifatemi@gmail.com>
2023-05-29 21:17:47 +0430

    Edit the overall structure

--- TODO/PresentationLayer/Main.cs
+++ TODO/PresentationLayer/Main.cs
@@ -1,18 +1,52 @@
-﻿namespace TODO;
+﻿using TODO.Models;
 
-public abstract class Main
+namespace TODO.PresentationLayer;
+
+public abstract class Main : Validation
 {
-    public static object SignupMethod(Signup signup) { return DataAccess.AddUser(signup); }
-    
-    public static object LoginMethod(Login login) { return DataAccess.EnterUser(login); }
-    
-    public static List<object> AllUsersMethod() { return DataAccess.GetAllUsers(); }
+    public static IResult SignupMethod(Signup signup)
+    {
+        if (!NotEmpty.CheckSignup(signup)) return Results.BadRequest(new { message = "Enter the requested items" });
+        if (CheckEmail(signup.email) && CheckName(signup.firstName, signup.lastName) &&
+            CheckUsername(signup.username) && CheckPassword(signup.password))
+            return Results.Ok(new { message = (string)BusinessLayer.Main.AddUser(signup) });
+        return Results.BadRequest(new { message = "Enter the parameters correctly" });
+    }
 
-    public static object CreateIssueMethod(Issue issue) { return DataAccess.AddIssue(issue); }
-    
-    public static List<object> AllIssuesMethod() { return DataAccess.GetAllIssue(); }
-    
-    public static object EditIssueMethod(Issue issue) { return DataAccess.EditIssue(issue); }
-    
-    public static object DeleteIssueMethod(int Id) { return DataAccess.DeleteIssue(Id); }
+    public static IResult LoginMethod(Login login)
+    {
+        if (!NotEmpty.CheckLogin(login)) return Results.BadRequest(new { message = "Enter the requested items" });
+        if (CheckUsername(login.username) && CheckPassword(login.password))
+            return Results.Ok(new { message = (string)BusinessLayer.Main.EnterUser(login) });
+        return Results.BadRequest(new { message = "Enter the parameters correctly" });
+    }
+
+    public static IResult AllUsersMethod()
+    {
+        return Results.Ok(new { message = BusinessLayer.Main.GetAllUsers() });
+    }
+
+    public static IResult CreateIssueMethod(Issue issue)
+    {
+        return NotEmpty.CheckIssue(issue)
+            ? Results.Ok(new { message = (string)BusinessLayer.Main.AddIssue(issue) })
+            : Results.BadRequest(new { message = "Enter the requested items" });
+    }
+
+    public static IResult AllIssuesMethod()
+    {
+        return Results.Ok(new { message = BusinessLayer.Main.GetAllIssue() });
+    }
+
+    public static IResult EditIssueMethod(Issue issue)
+    {
+        return NotEmpty.CheckIssue(issue)
+            ? Results.Ok(new { message = (string)BusinessLayer.Main.EditIssue(issue) })
+            : Results.BadRequest(new { message = "Enter the requested items" });
+    }
+
+    public static IResult DeleteIssueMethod(int id)
+    {
+        return Results.Ok(new { message = BusinessLayer.Main.DeleteIssue(id) });
+    }
 }
