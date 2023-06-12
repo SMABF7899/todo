9173cd908a78665772576bf1f6f87114be49063b
Seyed Mohammad Ali Bani Fatemi <smabanifatemi@gmail.com>
2023-06-12 20:32:21 +0430

    Add Exception for Database

--- TODO/BusinessLayer/Main.cs
+++ TODO/BusinessLayer/Main.cs
@@ -8,17 +8,25 @@ public abstract class Main
     public static bool AddUser(Signup signup)
     {
         if (DataAccessLayer.Main.FindUser(signup.username, signup.email)) return false;
-        var token = BCrypt.Net.BCrypt.HashPassword(signup.password);
-        signup.personalToken = token;
-        DataAccessLayer.Main.InsertUser(signup);
-        return true;
+        try
+        {
+            var token = BCrypt.Net.BCrypt.HashPassword(signup.password);
+            signup.personalToken = token;
+            var result = DataAccessLayer.Main.InsertUser(signup);
+            return result;
+        }
+        catch (Exception e) { throw new Exception(e.Message); }
     }
 
     public static string? EnterUser(Login login)
     {
-        DotEnv.Load();
-        var jwt = DataAccessLayer.Main.LoginUser(login);
-        return jwt != "" ? jwt : "";
+        try
+        {
+            DotEnv.Load();
+            var jwt = DataAccessLayer.Main.LoginUser(login);
+            return jwt != "" ? jwt : "";
+        }
+        catch (Exception e) { throw new Exception(e.Message); }
     }
 
     public static List<object> GetAllUsers()
@@ -44,8 +52,12 @@ public abstract class Main
 
     public static bool AddIssue(Issue issue)
     {
-        var result = DataAccessLayer.Main.InsertIssue(issue);
-        return result;
+        try
+        {
+            var result = DataAccessLayer.Main.InsertIssue(issue);
+            return result;
+        }
+        catch (Exception e) { throw new Exception(e.Message); }
     }
 
     public static List<object> GetAllIssue()
@@ -70,13 +82,21 @@ public abstract class Main
 
     public static bool EditIssue(Issue newIssue)
     {
-        var result = DataAccessLayer.Main.EditIssue(newIssue);
-        return result;
+        try
+        {
+            var result = DataAccessLayer.Main.EditIssue(newIssue);
+            return result;
+        }
+        catch (Exception e) { throw new Exception(e.Message); }
     }
 
     public static bool DeleteIssue(int id)
     {
-        var result = DataAccessLayer.Main.DeleteIssue(id);
-        return result;
+        try
+        {
+            var result = DataAccessLayer.Main.DeleteIssue(id);
+            return result;
+        }
+        catch (Exception e) { throw new Exception(e.Message); }
     }
 }
--- TODO/PresentationLayer/Main.cs
+++ TODO/PresentationLayer/Main.cs
@@ -11,10 +11,14 @@ public abstract class Main : Validation
             !CheckUsername(signup.username) ||
             !CheckPassword(signup.password))
             return Results.BadRequest(new { message = "Enter the parameters correctly" });
-        var result = BusinessLayer.Main.AddUser(signup);
-        return !result
-            ? Results.BadRequest(new { message = "Username or email is already registered" })
-            : Results.Ok(new { message = "Registration was successful" });
+        try
+        {
+            var result = BusinessLayer.Main.AddUser(signup);
+            return !result
+                ? Results.BadRequest(new { message = "Username or email is already registered" })
+                : Results.Ok(new { message = "Registration was successful" });
+        }
+        catch (Exception e) { return Results.BadRequest(new { message = "Error in Add User - 500 : " + e }); }
     }
 
     public static IResult LoginMethod(Login login)
@@ -22,10 +26,14 @@ public abstract class Main : Validation
         if (!NotEmpty.CheckLogin(login)) return Results.BadRequest(new { message = "Enter the requested items" });
         if (!CheckUsername(login.username) || !CheckPassword(login.password))
             return Results.BadRequest(new { message = "Enter the parameters correctly" });
-        var result = BusinessLayer.Main.EnterUser(login);
-        return result != ""
-            ? Results.Ok(new { message = "Login was successful", jwt = result })
-            : Results.BadRequest(new { message = "Username or password is not correct" });
+        try
+        {
+            var result = BusinessLayer.Main.EnterUser(login);
+            return result != ""
+                ? Results.Ok(new { message = "Login was successful", jwt = result })
+                : Results.BadRequest(new { message = "Username or password is not correct" });
+        }
+        catch (Exception e) { return Results.BadRequest(new { message = "Error in Login User - 500 : " + e }); }
     }
 
     public static IResult AllUsersMethod()
@@ -36,10 +44,14 @@ public abstract class Main : Validation
     public static IResult CreateIssueMethod(Issue issue)
     {
         if (!NotEmpty.CheckIssue(issue)) return Results.BadRequest(new { message = "Enter the requested items" });
-        var result = BusinessLayer.Main.AddIssue(issue);
-        return result
-            ? Results.Ok(new { message = "Issue has been successfully created" })
-            : Results.BadRequest(new { message = "Enter the requested items" });
+        try
+        {
+            var result = BusinessLayer.Main.AddIssue(issue);
+            return result
+                ? Results.Ok(new { message = "Issue has been successfully created" })
+                : Results.BadRequest(new { message = "Enter the requested items" });
+        }
+        catch (Exception e) { return Results.BadRequest(new { message = "Error in Add Issue - 500 : " + e }); }
     }
 
     public static IResult AllIssuesMethod()
@@ -50,17 +62,25 @@ public abstract class Main : Validation
     public static IResult EditIssueMethod(Issue issue)
     {
         if (!NotEmpty.CheckIssue(issue)) return Results.BadRequest(new { message = "Enter the requested items" });
-        var result = BusinessLayer.Main.EditIssue(issue);
-        return result
-            ? Results.Ok(new { message = "Issue has been updated" })
-            : Results.BadRequest(new { message = "Issue not found" });
+        try
+        {
+            var result = BusinessLayer.Main.EditIssue(issue);
+            return result
+                ? Results.Ok(new { message = "Issue has been updated" })
+                : Results.BadRequest(new { message = "Issue not found" });
+        }
+        catch (Exception e) { return Results.BadRequest(new { message = "Error in Edit Issue - 500 : " + e }); }
     }
 
     public static IResult DeleteIssueMethod(int id)
     {
-        var result = BusinessLayer.Main.DeleteIssue(id);
-        return result
-            ? Results.Ok(new { message = "Issue has been deleted" })
-            : Results.BadRequest(new { message = "Issue not found" });
+        try
+        {
+            var result = BusinessLayer.Main.DeleteIssue(id);
+            return result
+                ? Results.Ok(new { message = "Issue has been deleted" })
+                : Results.BadRequest(new { message = "Issue not found" });
+        }
+        catch (Exception e) { return Results.BadRequest(new { message = "Error in Delete Issue - 500 : " + e }); }
     }
 }
